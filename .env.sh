7b377e2965455f12d086fa61da6270e05d683213
Probe100 <1026657295@qq.com>
2025-02-14 13:19:22 +0800

    env.sh

---
+++ .env.sh
@@ -0,0 +1,6 @@
+export HF_HOME="/mnt/public/hanling/cache"
+export HF_ENDPOINT="https://hf-mirror.com"
+export HTTP_PROXY='http://10.12.3.4:1089'
+export HTTPS_PROXY='http://10.12.3.4:1089'
+export https_proxy='http://10.12.3.4:1089'
+export http_proxy='http://10.12.3.4:1089'
