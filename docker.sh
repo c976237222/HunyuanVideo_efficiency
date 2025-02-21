12137de5364f90e37398e1ead8ee7e4527a67ff0
Probe100 <1026657295@qq.com>
2025-02-21 15:37:15 +0800

    1

--- docker.sh
+++
@@ -1,5 +0,0 @@
-docker run --gpus all --rm -it \
-  -v /mnt/public/wangsiyuan/HunyuanVideo_efficiency:/home/anaconda/workspace \
-  -e VIDIA_DRIVER_CAPABILITIES=video\
-  docker-0.unsee.tech/xychelsea/ffmpeg-nvidia:latest \
-  /bin/bash
